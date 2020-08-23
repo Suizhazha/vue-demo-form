55a0adbec5fb03e1a3058a506747ac35f26ffc8f
Suizhazha <573505403@qq.com>
2020-08-23 15:37:17 +0800

    双向绑定，即修改视图中的input值，内存中的message会改变，通过按钮修改内存中的message值，视图中input值会改变

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <label>
+      <input v-model="message" placeholder="edit me">
+    </label>
+    <p>Message is: {{ message }}</p>
+    <p><button @click="message = 'suixin'"> set message to suixin</button></p>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
+  name: "App",
+  data(){
+     return {
+       message:'hi'
+     }
+  },
   components: {
-    HelloWorld
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
